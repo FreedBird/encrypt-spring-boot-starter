de7e70e55bebc244175a60d421efc336f5688725
何焱 <HY_Python@163.com>
2022-06-18 10:07:53 +0800

    提交代码

---
+++ src/main/java/com/tdcq/AppStartUp.java
@@ -0,0 +1,13 @@
+package com.tdcq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppStartUp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppStartUp.class, args);
+    }
+
+}
